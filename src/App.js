5cae99772aa45af944219f1bef5de58aa3ecb610
Harsh-winuall <harsh@winuall.com>
2024-08-09 12:26:44 +0530

    Releasing the new version v1.0.1

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
     <div>
       <h1>Heyya 👋🏻 Harsh here!</h1>
       <p>Welcome to my App - using Electron Updater </p>
+      <p>Note-: This is the Updated Version 😁</p>
       <hr />
-      <h3>Version : v1.0.0</h3>
+      <h3>Version : v1.0.1</h3>
     </div>
   )
 }
